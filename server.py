108e26f8f8aeaa66fc82fa810a12ff58689af8bc
Miquel Llobet <mllobet.cm@gmail.com>
2015-09-20 05:22:00 -0400

    Added spotify

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2.7
 
 from flask import Flask
-# from flask import request
+from flask import request
+from flask import jsonify
+
+import requests
 
 app = Flask(__name__)
 
@@ -29,6 +32,17 @@ def post_listening():
 @app.route('/song', methods=['GET'])
 def render_song():
     return 'OK'
+
+
+def get_spotify(link):
+    url = 'https://api.spotify.com/v1/search?q=' + link.replace(' ', '%20') + '&type=track'
+    res = requests.get(url)
+    items = res['tracks']['items']
+    if len(items) < 1:
+        return {'link': ''}
+    track = items['album']['external_urls']['spotify']
+    return {'link': track}
+
 
 if __name__ == "__main__":
 
